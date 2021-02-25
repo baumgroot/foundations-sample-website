6ed32d5ec050c670149b4187b244bee8936f8018
Markus <markusgarmeister@gmail.com>
2021-02-25 13:33:53 +0100

    added index route

--- sample_website/website.py
+++ sample_website/website.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/sample')
 def hello_world():
     return '<h1>Hello Döni!!!</h1>'
+
+
+@app.route('/<name>')
+def index(name=None):
+    return render_template('index.html', name=name)
 
 
 if __name__ == "__main__":
