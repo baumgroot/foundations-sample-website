53b29ef8715a1fd5a4dcba030827f43db73fbf6b
Markus <markusgarmeister@gmail.com>
2021-03-05 22:11:26 +0100

    put path in diffrent var

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -1,4 +1,3 @@
-import json
 # This file should contain a function called get_color_code().
 # This function should take one argument, a color name,
 # and it should return one argument, the hex code of the color,
@@ -6,15 +5,16 @@ import json
 # raise and handle an error that helps both you as a developer,
 # for example by logging the request and error, and the user,
 # letting them know that their color doesn't exist.
+import json
+import os
 
 
 def get_color_code(color_name):
     # this is where you should add your logic to check the color.
     # Open the file at data/css-color-names.json, and return the hex code
     # The file can be considered as JSON format, or as a Python dictionary.
-    color_list_path = open(
-        "/Users/markusgarmeister/desktop/Foundation21/css-color-names.json")
-
+    a = "/Users/markusgarmeister/desktop/Foundation21/foundations-sample-website/color_check/data/css-color-names.json"
+    color_list_path = open(a)
     color_dic = json.load(color_list_path)
 
     if color_name in color_dic.keys():
