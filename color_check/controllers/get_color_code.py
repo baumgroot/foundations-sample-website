ee0d8d768001f10289783319f92f92931951122f
Markus <markusgarmeister@gmail.com>
2021-03-05 21:32:24 +0100

    changed path

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -13,7 +13,8 @@ def get_color_code(color_name):
     # Open the file at data/css-color-names.json, and return the hex code
     # The file can be considered as JSON format, or as a Python dictionary.
     color_list_path = open(
-        '/Users/markusgarmeister/desktop/Foundation21/foundations-sample-website/color_check/data/css-color-names.json')
+        "/Users/markusgarmeister/desktop/Foundation21/css-color-names.json")
+
     color_dic = json.load(color_list_path)
 
     if color_name in color_dic.keys():
