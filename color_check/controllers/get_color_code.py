6405879638783cd58bcc652560ac143db7a04323
Markus <markusgarmeister@gmail.com>
2021-03-05 20:30:05 +0100

    import json

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -1,3 +1,4 @@
+import json
 # This file should contain a function called get_color_code().
 # This function should take one argument, a color name,
 # and it should return one argument, the hex code of the color,
@@ -5,6 +6,7 @@
 # raise and handle an error that helps both you as a developer,
 # for example by logging the request and error, and the user,
 # letting them know that their color doesn't exist.
+
 
 def get_color_code(color_name):
     # this is where you should add your logic to check the color.
